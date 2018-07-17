c14b8fc41e434ecc1fbb8ce6c2cd41ef4c2450dc
Oleksandr Boiko <mega.boich@gmail.com>
2018-07-17 21:45:12 +0200

    Application structure simplified

--- html-app/typings-custom/require.d.ts
+++
@@ -1,3 +0,0 @@
-//This is stub for require function so typescript will not show an error.
-//require function is used by webpack to build dependencies
-declare function require(string): any;
--- run-server.cmd
+++
@@ -1,1 +0,0 @@
-java -jar target/jwjs-test-0.1.0.jar
